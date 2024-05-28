383913192e5cf64ad14f0b58b909d29acfef0674
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-05-28 12:19:03 +0600

    make login page and make attractive

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 const Login = () => {
@@ -35,54 +36,59 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input
-            {...register("email", { required: true })}
-            type="email"
-            placeholder="Enter your email"
-            className="input input-bordered"
-          />
-          {errors.email && <span>This field is required</span>}
+    <div className="grid grid-cols-2 gap-10 items-center container mx-auto">
+      <img src="https://i.ibb.co/3TB6snz/scratch-bd-main-logo.png" alt="" />
+      <div className="w-full mx-auto border rounded-xl">
+        <form onSubmit={handleSubmit(onSubmit)} className="card-body ">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              {...register("email", { required: true })}
+              type="email"
+              placeholder="Enter your email"
+              className="input input-bordered"
+            />
+            {errors.email && <span>This field is required</span>}
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <div className="flex items-center gap-3 justify-between input input-bordered">
+            <input
+              {...register("password", { required: true })}
+              type={passShow ? "text" : "password"}
+              placeholder="enter your password"
+              className="w-full"
+              
+            />
+            <div className="text-xl text-blue-gray-400">
+                <span onClick={() => setPassShow(!passShow)}>
+                  
+                    {passShow ? <FaEyeSlash/> : <FaEye/>}
+                 
+                </span>
+              </div>
+            </div>
+            
+          </div>
+          <div className="form-control mt-1">
+            <button className="btn btn-warning">Login</button>
+          </div>
+        </form>
+        <div className="text-center mb-7 px-5">
+          <div className="divider"></div>
+          <p className="font-semibold">Or Sign In with</p>
+          <div className="flex items-center justify-center gap-4 my-2">
+            {/* <SocialLogin></SocialLogin> */}
+          </div>
+          <Link to="/signUp">
+            Do not have an account?{" "}
+            <span className="text-blue-500 font-bold">SignUp</span>{" "}
+          </Link>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input
-            {...register("password", { required: true })}
-            type={passShow ? "text" : "password"}
-            placeholder="enter your password"
-            className="input input-bordered"
-          />
-          <label className="label">
-            <a className="label-text-alt link link-hover">
-              <p onClick={() => setPassShow(!passShow)}>
-                <small>
-                  {passShow ? <span>Hide Pass</span> : <span>Show Pass</span>}
-                </small>
-              </p>
-            </a>
-          </label>
-        </div>
-        <div className="form-control mt-1">
-          <button className="btn btn-warning">Login</button>
-        </div>
-      </form>
-      <div className="text-center mb-7 px-5">
-        <div className="divider"></div>
-        <p className="font-semibold">Or Sign In with</p>
-        <div className="flex items-center justify-center gap-4 my-2">
-          {/* <SocialLogin></SocialLogin> */}
-        </div>
-        <Link to="/signUp">
-          Do not have an account?{" "}
-          <span className="text-blue-500 font-bold">SignUp</span>{" "}
-        </Link>
       </div>
     </div>
   );
