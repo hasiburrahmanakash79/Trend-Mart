ac010b697580c829bc2c9601fd1976f68f2121d9
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-05-28 17:25:57 +0600

    make social login option2

--- src/Pages/LandingPage/HomeCategory/HomeCategory.jsx
+++ src/Pages/LandingPage/HomeCategory/HomeCategory.jsx
@@ -4,71 +4,87 @@ const HomeCategory = () => {
   return (
     <div className="px-10 py-5">
       <div className="flex justify-between items-center">
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="text-center">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
           </div>
+          <p className="">Category Name</p>
         </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="text-center">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
           </div>
+          <p className="">Category Name</p>
         </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="text-center">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
           </div>
+          <p className="">Category Name</p>
         </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="text-center">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
           </div>
+          <p className="">Category Name</p>
         </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="text-center">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
           </div>
+          <p className="">Category Name</p>
         </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="text-center">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
           </div>
+          <p className="">Category Name</p>
         </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="text-center">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
           </div>
+          <p className="">Category Name</p>
         </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="text-center">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
           </div>
+          <p className="">Category Name</p>
         </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="text-center">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
           </div>
+          <p className="">Category Name</p>
         </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="text-center">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
           </div>
+          <p className="">Category Name</p>
         </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-          </div>
-        </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-          </div>
-        </div>
-        <div className="avatar">
-          <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-          </div>
-        </div>
+
       </div>
     </div>
   );
